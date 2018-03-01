60ece1b541e51dbfa0555a86f52a4bce64135dc7
Gregory Havenga <gregory.havenga@gmail.com>
2018-03-01 19:26:20 +0200

    Requires

--- main.rb
+++ main.rb
@@ -1,5 +1,11 @@
 #!env ruby
 
+require 'pry'
+require 'awesome_print'
+require './file_outputer.rb'
+require './file_reader.rb'
+require './algorithm.rb'
+
 if ARGV[0].nil? || ARGV[0].empty?
   puts 'Please input a valid file as an arguement'
   exit
